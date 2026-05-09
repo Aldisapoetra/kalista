5ef3c7ccf8b4f96454dec43fcf426bfb596f602b
aldisapoetra <aldi3191@gmail.com>
2026-05-10 07:38:37 +0800

    edit padding in contact section

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -5,12 +5,12 @@ export default function Contact() {
   return (
     <section
       id="contact"
-      className="from-teal/10 to-teal/95 bg-linear-90 from-0% to-100% px-30 py-24"
+      className="from-teal/10 to-teal/95 bg-linear-90 from-0% to-100% px-6 py-24 md:px-30"
     >
-      <div className="from-ice/5 to-teal/70 grid gap-8 rounded-3xl border border-white/10 bg-linear-90 from-0% to-100% p-10 lg:grid-cols-2">
+      <div className="from-ice/5 to-teal/70 grid gap-8 rounded-3xl border border-white/10 bg-linear-90 from-0% to-100% p-4 md:p-10 lg:grid-cols-2">
         <div>
           <h2 className="font-heading mb-8 text-4xl font-semibold">
-            Temuan Kami
+            Temukan Kami
           </h2>
           <div className="alamat mb-8 flex w-max gap-4">
             <div>
@@ -64,12 +64,11 @@ export default function Contact() {
             Hubungi via Whatsapp
           </Link>
         </div>
-        <div className="google-maps border-ice/20 h-full w-full overflow-hidden rounded-3xl border-10">
+        <div className="google-maps border-ice/20 h-full overflow-hidden rounded-3xl border-10">
           <iframe
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3778.888714573676!2d122.72267477473983!3d-4.838881495136689!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2da3cbc32d48debb%3A0xa498669a1dd1a509!2sKalista%20Frozen%20food!5e1!3m2!1sid!2sid!4v1777944754549!5m2!1sid!2sid"
             width="600"
             height="450"
-            // style={{ border: "1px" }}
             allowFullScreen
             loading="lazy"
             referrerPolicy="no-referrer-when-downgrade"
